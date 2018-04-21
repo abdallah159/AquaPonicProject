8772c3b1b68135db440e9beea9fe65c0fa2f3581
abdallah159 <abdallah.mohammed.fciscu@gmail.com>
2018-04-21 04:06:22 +0200

    Register data validation

--- app/src/main/java/com/example/mohammed/graduationproject/LoginActivity.java
+++ app/src/main/java/com/example/mohammed/graduationproject/LoginActivity.java
@@ -5,19 +5,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
     }
 
     public void onClickLogin(View view) {
 
-        Intent loginIntent = new Intent(LoginActivity.this, MainActivity.class);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+
+
+        /*Intent loginIntent = new Intent(LoginActivity.this, MainActivity.class);
         LoginActivity.this.startActivity(loginIntent);
-        LoginActivity.this.finish();
+        LoginActivity.this.finish();*/
 
 
     }
--- app/src/main/java/com/example/mohammed/graduationproject/RegisterActivity.java
+++ app/src/main/java/com/example/mohammed/graduationproject/RegisterActivity.java
@@ -4,25 +4,146 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RegisterActivity extends AppCompatActivity {
+    EditText useNameET, userAdressET, userEmailET, userPhoneET, userJopET, userPasswordET;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        useNameET = findViewById(R.id.userName);
+        userAdressET = findViewById(R.id.userAdress);
+        userEmailET = findViewById(R.id.userMail);
+        userPhoneET = findViewById(R.id.userMobile);
+        userJopET = findViewById(R.id.userJop);
+        userPasswordET = findViewById(R.id.userPassword);
+
+
     }
 
     public void onClickRegister(View view) {
-        Intent loginIntent = new Intent(RegisterActivity.this, LoginActivity.class);
+        dataValidation();
+       /* Intent loginIntent = new Intent(RegisterActivity.this, LoginActivity.class);
         RegisterActivity.this.startActivity(loginIntent);
-        RegisterActivity.this.finish();
+        RegisterActivity.this.finish();*/
     }
 
     public void onClickLogin(View view) {
-        Intent loginIntent = new Intent(RegisterActivity.this, LoginActivity.class);
+      /*  Intent loginIntent = new Intent(RegisterActivity.this, LoginActivity.class);
         RegisterActivity.this.startActivity(loginIntent);
-        RegisterActivity.this.finish();
+        RegisterActivity.this.finish();*/
+
 
     }
+
+
+    public void dataValidation() {
+
+        //Validation of Data to register
+        if (useNameET.getText().toString().length() == 0)
+            useNameET.setError("Field cannot be left empty!");
+
+        else if (userPasswordET.getText().toString().length() == 0)
+            userPasswordET.setError("Field cannot be left empty!");
+
+        else if (userJopET.getText().toString().length() == 0)
+            userJopET.setError("Field cannot be left empty!");
+
+        else if (userPhoneET.getText().toString().length() == 0)
+            userPhoneET.setError("Field cannot be left empty!");
+
+        else if (userEmailET.getText().toString().length() == 0)
+            userEmailET.setError("Field cannot be left empty!");
+
+        else if (userAdressET.getText().toString().length() == 0)
+            userAdressET.setError("Field cannot be left empty!");
+
+        else if (!isEmailValid(userEmailET.getText().toString()))
+            userEmailET.setError("Please enter valid email adress");
+
+        else if (!isValidMobile(userPhoneET.getText().toString()))
+            userPhoneET.setError("Please enter valid mobile number");
+
+        else if (userPasswordET.getText().toString().length() < 8)
+            userPasswordET.setError("Password must be minimum 8 symbols");
+
+        else if(!isValidString(userAdressET.getText().toString()))
+            userAdressET.setError("Please enter valid adress");
+
+        else if(!isValidString(userJopET.getText().toString()))
+            userJopET.setError("Please enter valid jop");
+
+        else if(!isValidString(useNameET.getText().toString()))
+            useNameET.setError("Please enter valid name");
+
+        else{
+            //handle Register here
+            registerUser();
+        }
+
+
+
+    }
+
+    private boolean registerUser() {
+
+        return true ;
+
+    }
+
+    public boolean isEmailValid(String email) {
+        String regExpn =
+                "^(([\\w-]+\\.)+[\\w-]+|([a-zA-Z]{1}|[\\w-]{2,}))@"
+                        + "((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                        + "[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\."
+                        + "([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                        + "[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
+                        + "([a-zA-Z]+[\\w-]+\\.)+[a-zA-Z]{2,4})$";
+
+        CharSequence inputStr = email;
+
+        Pattern pattern = Pattern.compile(regExpn, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(inputStr);
+
+        if (matcher.matches())
+            return true;
+        else
+            return false;
+    }
+
+    private boolean isValidMobile(String phone) {
+        boolean check = false;
+        if (!Pattern.matches("[a-zA-Z]+", phone)) {
+            if (phone.length() < 6 || phone.length() > 13) {
+                // if(phone.length() != 10) {
+                check = false;
+            } else {
+                check = true;
+            }
+        } else {
+            check = false;
+        }
+        return check;
+    }
+
+
+    public static boolean isValidString(String input)
+    {
+        try {
+            int i = Integer.parseInt(input);
+            return false;
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+            return true;
+        }
+    }
+
 }
